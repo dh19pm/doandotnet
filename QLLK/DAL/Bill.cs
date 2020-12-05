9d49293b6e531a5c46330904dcde15f386cae5fe
Sĩ Ben <dangtiensi.vn@gmail.com>
2020-12-05 08:45:45 +0700

    Summary

---
+++ QLLK/DAL/Bill.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLLK.DAL
+{
+    class Bill
+    {
+        Connect connect = new Connect();
+        public Bill()
+        {
+
+        }
+        public DataTable Get()
+        {
+            return connect.Query("SELECT * FROM BILL").Fetch();
+        }
+        public void Insert(string[] value)
+        {
+            connect.Query("INSERT INTO BILL(AMOUNT, PRICE, CREATE_DATE) VALUES(?, ?, ?)").Value(value);
+        }
+        public void Update(string[] value)
+        {
+            connect.Query("UPDATE BILL SET AMOUNT = ?, PRICE = ?, CREATE_DATE = ? WHERE ID = ?").Value(value);
+        }
+    }
+}
---
+++ QLLK/DAL/Connect.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Data;
+
+namespace QLLK.DAL
+{
+    class Connect
+    {
+        private string user = "sa";
+        private string pass = "1803";
+        private string db = "QLLK";
+        private string instance = "SIBEN\\SIBEN";
+        private SqlConnection connect;
+        private SqlCommand cmd;
+        private string[] parameters;
+        public Connect()
+        {
+            try
+            {
+                this.connect = new SqlConnection("Data Source=" + instance + ";Initial Catalog=" + db + ";User ID=" + user + ";Password=" + pass);
+                this.connect.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public Connect Query(string sql)
+        {
+            if (this.connect.State == System.Data.ConnectionState.Open)
+            {
+                parameters = sql.Trim().Split('?');
+                if(parameters.Length > 0)
+                {
+                    sql = "";
+                    for(int i = 0; i < parameters.Length; i++)
+                    {
+                        if (i == parameters.Length - 1)
+                            sql += parameters[i];
+                        else
+                            sql += parameters[i] + "@param" + (i + 1);
+                    }
+                }
+                cmd = new SqlCommand(sql, this.connect);
+                return this;
+            }
+            return this;
+        }
+        public void Execute()
+        {
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+        }
+        public DataTable Fetch()
+        {
+            DataTable data = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(data);
+            adapter.Dispose();
+            return data;
+        }
+        public void Value(string[] array)
+        {
+            if (array.Length == (parameters.Length - 1))
+            {
+                for (int i = 0; i < array.Length; i++)
+                    cmd.Parameters.AddWithValue("@param" + (i + 1), array[i]);
+                cmd.ExecuteNonQuery();
+            }
+        }
+        ~Connect()
+        {
+            if (this.connect.State != System.Data.ConnectionState.Open)
+                this.connect.Close();
+        }
+    }
+}
